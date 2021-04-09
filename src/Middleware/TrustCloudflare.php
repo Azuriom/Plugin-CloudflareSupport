ec257775275ce9bf70e7f04e93e8b357c4f241f2
MrMicky <git@mrmicky.fr>
2021-04-09 11:07:59 +0200

    Update Cloudflare IPs ranges

--- src/Middleware/TrustCloudflare.php
+++ src/Middleware/TrustCloudflare.php
@@ -15,20 +15,21 @@ class TrustCloudflare
      * @var array
      */
     protected $proxies = [
-        '173.245.48.0/20',
         '103.21.244.0/22',
         '103.22.200.0/22',
         '103.31.4.0/22',
-        '141.101.64.0/18',
+        '104.16.0.0/13',
+        '104.24.0.0/14',
         '108.162.192.0/18',
-        '190.93.240.0/20',
+        '131.0.72.0/22',
+        '141.101.64.0/18',
+        '162.158.0.0/15',
+        '172.64.0.0/13',
+        '173.245.48.0/20',
         '188.114.96.0/20',
+        '190.93.240.0/20',
         '197.234.240.0/22',
         '198.41.128.0/17',
-        '162.158.0.0/15',
-        '104.16.0.0/12',
-        '172.64.0.0/13',
-        '131.0.72.0/22',
         '2400:cb00::/32',
         '2606:4700::/32',
         '2803:f800::/32',
@@ -45,7 +46,7 @@ class TrustCloudflare
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         // We use CF-Connecting-IP instead of X-Forwarded-For since
         // it has a consistent format containing only one IP.
